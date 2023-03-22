394b29be6edb05a1cdab2b70ee22db9dde94b622
gonplz <gonzaplazaola@gmai.com>
2023-03-21 21:38:13 -0300

    Finalizando index

---
+++ assets/javascript/index.js
@@ -0,0 +1,49 @@
+const { createApp } = Vue
+
+const urlApi = "https://mindhub-xj03.onrender.com/api/amazing"
+
+createApp({
+    data() {
+        return {
+            dataEvents: [],
+            dataCheck: [],
+            inputCheck: [],
+            stringSearch: "",
+            resultFilter:[]
+        }
+    },
+    created() {
+
+        fetch(urlApi)
+            .then(response => response.json())
+            .then(data => {
+                // console.log(data.events)
+                this.dataEvents = data.events
+                console.log(this.dataEvents)
+
+                let arrayCate = this.dataEvents.map(evento => evento.category)
+
+                this.dataCheck = arrayCate.filter((item, index) => {
+                    return arrayCate.indexOf(item) === index;
+                })
+                console.log(this.dataCheck)
+            })
+    },
+    methods: {
+
+    },
+    computed: {
+
+        filterCheck: function () {
+            let filterCategory = this.inputCheck.length == 0 ? this.dataEvents : this.dataEvents.filter(elemento => this.inputCheck.includes(elemento.category))
+            this.resultFilter = this.stringSearch == "" ? filterCategory : filterCategory.filter(elemento => elemento.name.toLowerCase().includes(this.stringSearch.toLowerCase().trim()))
+        }
+    }
+}).mount("#app")
+
+
+// Segunda forma de hacer la funcion de filtros cruzados
+// filterCheck: function () {
+//     let filterCategory = this.inputCheck.length == 0 ? this.dataEvents : this.dataEvents.filter(elemento => this.inputCheck.includes(elemento.category))
+//     return this.stringSearch == "" ? filterCategory : filterCategory.filter(elemento => elemento.name.toLowerCase().includes(this.stringSearch.toLowerCase().trim()))
+// }
